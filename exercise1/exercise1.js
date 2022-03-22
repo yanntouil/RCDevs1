9f5d9c3c2029df317b04f0f49a1646dfe7305a94
fabien <61295d>
2022-03-22 16:18:06 +0100

    exercise 1

---
+++ exercise1/exercise1.js
@@ -0,0 +1,27 @@
+const UI = {};
+
+UI.Icon = function( src ) {
+	var src = src;
+	this.src = function() {
+		return src;					
+	};
+};
+
+UI.MenuItem = function(properties) {
+
+	this.add = function(menuItem) {
+
+	};
+
+};
+UI.Menu = function() {
+
+	this.add = function(menuItem) {
+
+	};
+
+	this.appendTo = function(DOMElement) {
+
+	};
+
+};
